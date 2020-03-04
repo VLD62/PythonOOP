048a7a17e8e99fcf557cd893f13e406921738e31
Vladislav Iliev <viliev2@dxc.com>
2020-03-04 11:36:36 +0200

    Add tasks from Exercise 02. Classes and Instances

---
+++ 02-Classes-and-Instances/Exercise/05. Time.py
@@ -0,0 +1,42 @@
+class Time:
+    max_hours = 24
+    max_minutes = 60
+    max_seconds = 60
+
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def set_time(self, hours: int, minutes: int, seconds: int):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def get_time(self):
+        return f"{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}"
+
+    def next_second(self):
+        if self.seconds + 1 >= Time.max_seconds:
+            self.seconds = 0
+            if self.minutes + 1 >= Time.max_minutes:
+                self.minutes = 0
+                if self.hours + 1 > Time.max_hours:
+                    self.hours = 1
+                else:
+                    self.hours += 1
+            else:
+                self.minutes += 1
+        else:
+            self.seconds += 1
+        return self.get_time()
+
+
+time = Time(9, 30, 60)
+print(time.next_second())
+
+time = Time(10, 59, 59)
+print(time.next_second())
+
+time = Time(24, 59, 59)
+print(time.next_second())
